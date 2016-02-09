ec576662fc6b89a9304fb909bfdd3b6c981d66a8
Andrew Downing <ad510n1@gmail.com>
2016-02-08 17:26:11 -0800

    netbeans uses 4 space indent

--- src/main/java/csci599/App.java
+++ src/main/java/csci599/App.java
@@ -5,10 +5,10 @@ import org.apache.tika.Tika;
 
 // trying out code on Tika in Action pg 33
 public class App {
-	public static void main(String[] args) throws Exception {
-		Tika tika = new Tika();
-		for (String file : args) {
-			System.out.println(tika.parseToString(new File(file)));
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        Tika tika = new Tika();
+        for (String file : args) {
+            System.out.println(tika.parseToString(new File(file)));
+        }
+    }
 }
