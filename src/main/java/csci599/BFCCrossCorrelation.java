f15edb4a9ed9ba6743d988d6c48ca585eb2f0136
Dipali Bhatt <dbhatt@usc.edu>
2016-02-28 04:16:38 -0800

    change in BFCCrossCorrelation

--- src/main/java/csci599/BFCCrossCorrelation.java
+++ src/main/java/csci599/BFCCrossCorrelation.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,17 +15,14 @@ public class BFCCrossCorrelation
 {
     static int noOfMIMETypes;
     static int charSetSize;
-    static ArrayList<String> MIMETypes;
+    static List<String> MIMETypes;
     static HashMap<String,ArrayList<ArrayList<Double>>> CorrMatrix;
     
-    BFCCrossCorrelation()
+    BFCCrossCorrelation(File sortFolder)
     {
         noOfMIMETypes=15;
         charSetSize=256;
-        MIMETypes=new ArrayList<String>();
-        MIMETypes.add("image/jpg");
-        MIMETypes.add("image/png");
-        MIMETypes.add("text/plain");
+        MIMETypes=FileTypeFilter.getMIMETypes(sortFolder);
         CorrMatrix=new HashMap<String,ArrayList<ArrayList<Double>>>();
         ArrayList<Double> tempAL1=new ArrayList<Double>();
         ArrayList<ArrayList<Double>> tempAL2=new ArrayList<ArrayList<Double>>();
@@ -39,13 +37,12 @@ public class BFCCrossCorrelation
             }
             tempAL2.add(tempAL1);
         }
-        Iterator i=MIMETypes.iterator();
-        while(i.hasNext())
-            CorrMatrix.put((String) i.next(), tempAL2);
+        for(String s:MIMETypes)
+            CorrMatrix.put(s, tempAL2);
     }
     
     public void listFilesForFolder(final File folder) throws IOException, FileNotFoundException, IOException{
-        FileTypeFilter.forEach(folder,MIMETypes, (file, MIMEType) -> {
+        FileTypeFilter.forEach(folder, (file, MIMEType) -> {
             double[] BFD=new double[charSetSize];
             try {
                 BFD=frequencyDist(file);
@@ -67,9 +64,7 @@ public class BFCCrossCorrelation
         FileInputStream in = new FileInputStream(file);
         int c=0;
         while((c=in.read())!=-1)
-        {
             BFD[c]++;
-        }
         return BFD;
     }
     
@@ -79,9 +74,7 @@ public class BFCCrossCorrelation
         for(int j=1;j<matrix.length;j++)
         {
             for(int i=0;i<j;i++)
-            {
                 matrix[j][i]=BFD[j]-BFD[i];
-            }
         }
         return matrix;
     }
@@ -91,9 +84,7 @@ public class BFCCrossCorrelation
         for(int j=1;j<matrix.length;j++)
         {
             for(int i=j+1;j<matrix[j].length;i++)
-            {
                 matrix[j][i]=CorrelationStrength(matrix[i][j]);
-            }
         }
         return matrix;
     }
@@ -110,9 +101,7 @@ public class BFCCrossCorrelation
         for(int j=0;j<matrix.length;j++)
         {
             for(int i=0;i<matrix[j].length;i++)
-            {
                 tempMatrix[j][i]=matrixAL.get(j).get(i);
-            }
         }
         int noOfFiles=(int) tempMatrix[0][0];
         for(int j=0;j<matrix.length;j++)
@@ -126,10 +115,17 @@ public class BFCCrossCorrelation
         for(int j=0;j<matrix.length;j++)
         {
             for(int i=0;i<j;i++)
-            {
                 tempMatrix[j][i]=((tempMatrix[j][i]*noOfFiles)+matrix[j][i])/(noOfFiles+1);
-            }
         }
         tempMatrix[0][0]++;
+        ArrayList<ArrayList<Double>> AL1=new ArrayList<ArrayList<Double>>();
+        for(int j=0;j<tempMatrix.length;j++)
+        {
+            ArrayList<Double> AL2=new ArrayList<Double>();
+            for(int i=0;i<tempMatrix[j].length;i++)
+                AL2.add(matrix[j][i]);
+            AL1.add(AL2);
+        }
+        CorrMatrix.replace(MIMEType,AL1);
     }
 }
