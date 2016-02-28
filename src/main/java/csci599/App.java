b664533e9de5577444492ac14276a983a467c81f
Andrew Downing <ad510n1@gmail.com>
2016-02-27 18:11:24 -0800

    consistently read polar dump folder from command line

--- src/main/java/csci599/App.java
+++ src/main/java/csci599/App.java
@@ -5,16 +5,22 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) {
-        String usage = "Pass bfa to perform byte frequency analysis, bfc to perform byte frequency correlation, or fht to perform file/header trailer analysis.";
-        if (args.length < 1) {
-            System.out.println(usage);
+        String usage = "Command line arguments:\n" +
+            "bfa <folder>\n" +
+            "    Perform byte frequency analysis on files in <folder>.\n" +
+            "bfc <folder>\n" +
+            "    Perform byte frequency correlation on files in <folder>.\n" +
+            "fht <folder> <mime types>\n" +
+            "    Perform file header/trailer analysis on files in <folder> of specified <mime types>.\n";
+        if (args.length < 2) {
+            System.out.print(usage);
             return;
         }
         switch (args[0]) {
             case "bfa":
             {
                 BFA bf = new BFA();
-                final File folder = new File("../polar-dump");
+                final File folder = new File(args[1]);
                 bf.listFilesForFolder(folder);
                 break;
             }
@@ -22,7 +28,7 @@ public class App {
             case "bfc":
             {
                 BFDCorrelation bfc5a = new BFDCorrelation();
-                final File folder = new File("../polar-dump");
+                final File folder = new File(args[1]);
                 try {
                     bfc5a.listFilesForFolder(folder);
                 } catch (Exception ex) {
@@ -32,14 +38,10 @@ public class App {
                 break;
             }
             case "fht":
-                if (args.length < 2) {
-                    System.out.println("Pass in folder then list of content types");
-                    return;
-                }
                 FHT.analyze(new File(args[1]), Arrays.asList(Arrays.copyOfRange(args, 2, args.length)));
                 break;
             default:
-                System.out.println(usage);
+                System.out.print(usage);
         }
     }
 }
