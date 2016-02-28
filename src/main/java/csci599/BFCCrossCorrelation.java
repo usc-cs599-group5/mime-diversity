ccfe819c57778faa7715d84fe49290019463fd22
Dipali Bhatt <dbhatt@usc.edu>
2016-02-27 22:17:44 -0800

    chage in BFCCrossCorrelation

--- src/main/java/csci599/BFCCrossCorrelation.java
+++ src/main/java/csci599/BFCCrossCorrelation.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,34 +26,110 @@ public class BFCCrossCorrelation
         MIMETypes.add("image/png");
         MIMETypes.add("text/plain");
         CorrMatrix=new HashMap<String,ArrayList<ArrayList<Double>>>();
+        ArrayList<Double> tempAL1=new ArrayList<Double>();
+        ArrayList<ArrayList<Double>> tempAL2=new ArrayList<ArrayList<Double>>();
+        for(int j=0;j<charSetSize;j++)
+        {
+            for(int i=0;i<charSetSize;i++)
+            {
+                if(i>j)
+                    tempAL1.add(1.0);
+                else
+                    tempAL1.add(0.0);
+            }
+            tempAL2.add(tempAL1);
+        }
+        Iterator i=MIMETypes.iterator();
+        while(i.hasNext())
+            CorrMatrix.put((String) i.next(), tempAL2);
     }
     
-    public void listFilesForFolder(final File folder) throws IOException{
+    public void listFilesForFolder(final File folder) throws IOException, FileNotFoundException, IOException{
         FileTypeFilter.forEach(folder,MIMETypes, (file, MIMEType) -> {
-            ArrayList<Double> BFD=new ArrayList<Double>();
+            double[] BFD=new double[charSetSize];
             try {
                 BFD=frequencyDist(file);
-            } catch (FileNotFoundException ex) {
-                Logger.getLogger(BFCCrossCorrelation.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
             }
-            ArrayList<ArrayList<Double>> matrix=new ArrayList<ArrayList<Double>>();
+            double[][] matrix=new double[noOfMIMETypes][charSetSize];
+            matrix=generateCFMatrix(BFD);
+            matrix=calculateCS(matrix);
+            updateFPMatrix(matrix,MIMEType);
             //matrix=generateCorrMatrix(file);
         });        
     }
     
-    public ArrayList<Double> frequencyDist(File file) throws FileNotFoundException
+    public double[] frequencyDist(File file) throws IOException,FileNotFoundException, IOException
     {
-        ArrayList<Double> BFD=new ArrayList<Double>();
-        /*for(int i=0;i<charSetSize;i++)
-            BFD.add(0.0);
+        double[] BFD=new double[charSetSize];
+        for(int i=0;i<BFD.length;i++)
+            BFD[i]=0.0;
         FileInputStream in = new FileInputStream(file);
         int c=0;
         while((c=in.read())!=-1)
         {
-            double i=BFD.get(c);
-            BFD.remove(c);
-            //BFD.
-        }*/
+            BFD[c]++;
+        }
         return BFD;
+    }
+    
+    public double[][] generateCFMatrix(double[] BFD)
+    {
+        double[][] matrix=new double[charSetSize][charSetSize];
+        for(int j=1;j<matrix.length;j++)
+        {
+            for(int i=0;i<j;i++)
+            {
+                matrix[j][i]=BFD[j]-BFD[i];
+            }
+        }
+        return matrix;
+    }
+    
+    public double[][] calculateCS(double[][] matrix)
+    {
+        for(int j=1;j<matrix.length;j++)
+        {
+            for(int i=j+1;j<matrix[j].length;i++)
+            {
+                matrix[j][i]=CorrelationStrength(matrix[i][j]);
+            }
+        }
+        return matrix;
+    }
+    
+    public double CorrelationStrength(double x)
+    {
+        return 1-Math.abs(x);
+    }
+    
+    public void updateFPMatrix(double[][] matrix,String MIMEType)
+    {
+        ArrayList<ArrayList<Double>> matrixAL=new ArrayList<ArrayList<Double>>(CorrMatrix.get(MIMEType));
+        double[][] tempMatrix=new double[charSetSize][charSetSize];
+        for(int j=0;j<matrix.length;j++)
+        {
+            for(int i=0;i<matrix[j].length;i++)
+            {
+                tempMatrix[j][i]=matrixAL.get(j).get(i);
+            }
+        }
+        int noOfFiles=(int) tempMatrix[0][0];
+        for(int j=0;j<matrix.length;j++)
+        {
+            for(int i=j+1;i<matrix[j].length;j++)
+            {
+                double newCS=CorrelationStrength(tempMatrix[i][j]-matrix[i][j]);
+                matrix[j][i]=((tempMatrix[j][i]*noOfFiles)+newCS)/(noOfFiles+1);
+            }
+        }
+        for(int j=0;j<matrix.length;j++)
+        {
+            for(int i=0;i<j;i++)
+            {
+                tempMatrix[j][i]=((tempMatrix[j][i]*noOfFiles)+matrix[j][i])/(noOfFiles+1);
+            }
+        }
+        tempMatrix[0][0]++;
     }
 }
